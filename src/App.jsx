6733d441732eb5a81c393d310b4d88a648d1fdff
Nikita Malienko <nekmalienko2001@gmail.com>
2024-02-07 22:46:35 +0200

    added simple counter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import Header from './components/Header'
-import Form from './components/Form'
-import MenuList from './components/MenuList'
+import Header from "./components/Header";
+import Form from "./components/Form";
+import MenuList from "./components/MenuList";
+import Counter from "./components/Counter";
 
 function App() {
-
   return (
     <div className="wrapper">
       <Header />
       <main className="content">
-        <h1 className="title">The best pizza.<br /><span className="text-yellow">Straight out of the oven, straight to you.</span>
+        <h1 className="title">
+          The best pizza.
+          <br />
+          <span className="text-yellow">
+            Straight out of the oven, straight to you.
+          </span>
         </h1>
-        <p className="sub-title">👋 Welcome! Please start by telling us your name:</p>
+        <p className="sub-title">
+          👋 Welcome! Please start by telling us your name:
+        </p>
         <Form />
+        <Counter />
         <MenuList />
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
